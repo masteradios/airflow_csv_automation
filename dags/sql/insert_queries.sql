761fc074fb5e2d6a83159874d0e712c984f4e274
masteradios <reachadikush@gmail.com>
2024-09-30 19:18:50 +0530

    Initial commit

---
+++ dags/sql/insert_queries.sql
@@ -0,0 +1,3 @@
+INSERT INTO sample_table (id, name, age) VALUES (1, 'John Doe', 30);
+INSERT INTO sample_table (id, name, age) VALUES (2, 'Jane Smith', 25);
+INSERT INTO sample_table (id, name, age) VALUES (3, 'Bob Johnson', 22);
